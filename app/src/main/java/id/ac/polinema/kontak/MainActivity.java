77d18a2e1c388621f34d18938674e8232d27eef3
ErmiP <ermi.pristiya@gmail.com>
2019-11-21 15:05:09 +0700

    kontak_android

---
+++ app/src/main/java/id/ac/polinema/kontak/MainActivity.java
@@ -0,0 +1,74 @@
+package id.ac.polinema.kontak;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.ac.polinema.kontak.Adapter.AdapterKontak;
+import id.ac.polinema.kontak.InsertActivity;
+import id.ac.polinema.kontak.Model.GetKontak;
+import id.ac.polinema.kontak.Model.Kontak;
+import id.ac.polinema.kontak.R;
+import id.ac.polinema.kontak.Rest.ApiClient;
+import id.ac.polinema.kontak.Rest.ApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    Button btIns;
+    ApiInterface mApiInterface;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public static MainActivity ma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btIns = (Button) findViewById(R.id.btIns);
+        btIns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, InsertActivity.class));
+            }
+        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        ma=this;
+        refresh();
+    }
+
+    public void refresh() {
+        Call<GetKontak> kontakCall = mApiInterface.getKontak();
+        kontakCall.enqueue(new Callback<GetKontak>() {
+            @Override
+            public void onResponse(Call<GetKontak> call, Response<GetKontak>
+                    response) {
+                List<Kontak> KontakList = response.body().getListDataKontak();
+                Log.d("Retrofit Get", "Jumlah data Kontak: " +
+                        String.valueOf(KontakList.size()));
+                mAdapter = new AdapterKontak(KontakList);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<GetKontak> call, Throwable t) {
+                Log.e("Retrofit Get", t.toString());
+            }
+        });
+    }
+}
